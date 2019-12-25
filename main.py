a18a37be7a877d3e20a9b6cc8d915db3ea87e4ad
amit223 <amit.moshe100@gmail.com>
2019-12-25 13:03:33 +0200

    added function to get all important dates

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Biography import BiograpyGetter
 
+sorted_list_of_dates=[]
+singer_picture=None
 def funcTester():
     singer="Justin_Timberlake"
     url="https://en.wikipedia.org/wiki/"+singer
@@ -19,7 +21,31 @@ def funcTester():
 
 #this function opens gui and show singer's life
 def main():
-    x=5
+    sorted_list_of_dates = get_dates("Justin Timberlake")
+    
 
 
-funcTester()
+def get_dates(singer_name):
+    names=singer_name.split()
+    full_name=names[0]+"_"+names[1]
+    url = "https://en.wikipedia.org/wiki/" + full_name
+    discography_url = "https://en.wikipedia.org/wiki/" + full_name + "_discography"
+    bio = BiograpyGetter(url, discography_url)
+    #get all info about singer
+    singer_picture=bio.getSingerPicture()
+    birthdate=bio.getSingerBirthDate()
+    spouses=bio.getSingerSpouses()
+    singles=bio.getSingerSingles()
+    albums=bio.getSingerStudioAlbums()
+    list=[]
+    if len(birthdate)>0:
+        list.append(birthdate)
+    if len(spouses) > 0:
+        list.extend(spouses)
+    if len(singles) > 0:
+        list.extend(singles)
+    if len(albums) > 0:
+        list.extend(albums)
+    list.sort(key=lambda x: x[0])
+    return list
+
